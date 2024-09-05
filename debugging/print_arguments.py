056a7523b1d60b4c06a0bb028505d4efbce8732d
chutima puthachon <chutima.p8@gmail.com>
2024-09-05 21:44:45 +1000

    added print_arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 import sys
-import os
-# Print only the script name
-print(os.path.basename(sys.argv[0]))
-# Print the remaining arguments
+
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
